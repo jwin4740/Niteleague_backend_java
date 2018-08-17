c56bab4104b2b63a19df05620f25c38968e9abb0
james <jwin4740@gmail.com>
2018-08-17 11:41:47 -0400

    teams data inserting from txt filegit add .git add .

--- src/main/java/com/winkle/Niteleague/bootstrap/TeamLoader.java
+++ src/main/java/com/winkle/Niteleague/bootstrap/TeamLoader.java
@@ -1,4 +1,5 @@
 package com.winkle.Niteleague.bootstrap;
+
 import com.winkle.Niteleague.model.Team;
 import com.winkle.Niteleague.repository.TeamRepository;
 import org.apache.logging.log4j.LogManager;
@@ -7,6 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Component
 public class TeamLoader implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -14,7 +21,7 @@ public class TeamLoader implements ApplicationListener<ContextRefreshedEvent> {
     private TeamRepository teamRepository;
 
     private Logger log = LogManager.getLogger();
-//    private Logger log = Logger.getLogger(TeamLoader.class);
+
 
     @Autowired
     public void setTeamRepository(TeamRepository teamRepository) {
@@ -23,12 +30,56 @@ public class TeamLoader implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        // The name of the file to open.
+        String fileName = "teams.txt";
+        ArrayList<String> k = setProps(fileName);
+        for (String i : k) {
+            Team team = new Team();
+            team.setTeamName(i);
+            team.setAvatar(i + ".png");
+            teamRepository.save(team);
 
-        Team team = new Team();
-        team.setTeamName("Burr");
-        team.setAvatar("somethingg.png");
-        teamRepository.save(team);
+            log.info("Saved team");
+        }
+    }
 
-        log.info("Saved Shirt - id: " + team.getId());
+    private ArrayList<String> setProps(String fileName) {
+        // This will reference one line at a time
+        String line = null;
+        ArrayList<String> l = new ArrayList<>();
+
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                l.add(line);
+                System.out.println(line);
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        } catch (IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+
+
+        return l;
+
+
     }
 }
