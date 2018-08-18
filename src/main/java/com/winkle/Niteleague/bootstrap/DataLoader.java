21e02f3036f0da654581ba30ffdfcbfc978c277b
james <jwin4740@gmail.com>
2018-08-17 23:47:58 -0400

    added txt files with random data and made simple create rand passwd method

--- src/main/java/com/winkle/Niteleague/bootstrap/DataLoader.java
+++ src/main/java/com/winkle/Niteleague/bootstrap/DataLoader.java
@@ -2,6 +2,7 @@ package com.winkle.Niteleague.bootstrap;
 
 import com.winkle.Niteleague.model.Team;
 import com.winkle.Niteleague.repository.TeamRepository;
+import com.winkle.Niteleague.repository.UserRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +17,13 @@ import java.util.Map;
 
 @Component
 public class TeamLoader implements ApplicationListener<ContextRefreshedEvent> {
-
-
     private TeamRepository teamRepository;
-
+    private UserRepository userRepository;
     private Logger log = LogManager.getLogger();
-
-
     @Autowired
     public void setTeamRepository(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
     }
-
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         // The name of the file to open.
